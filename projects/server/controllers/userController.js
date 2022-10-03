07d8e53fc1d3b60514b2b50db8467d8aba5f947d
Abdi-01 <abdialghi@gmail.com>
2022-10-03 19:37:08 +0700

    feat : create token

--- projects/server/controllers/userController.js
+++ projects/server/controllers/userController.js
@@ -2,6 +2,7 @@ import db from "../database/models";
 const users = db.users;
 import { validationResult } from 'express-validator';
 import Crypto from 'crypto';
+import { createToken } from '../middleware/encript';
 
 export const getUsers = async (req, res, next) => {
     try {
@@ -23,16 +24,27 @@ export const auth = async (req, res, next) => {
         if (!validation.isEmpty()) {
             res.status(400).send(validation.errors)
         } else {
+            // 1. Hashing Password
+            req.body.password = Crypto.createHmac("sha256", process.env.HASH_KEY)
+                .update(req.body.password).digest("hex");
+
+            // 2. get data user
             let login = await users.findAll({
                 where: {
                     username: req.body.username,
                     password: req.body.password
-                }
+                },
+                attributes: ['iduser', 'username', 'email', 'imgProfile']
             })
+
+            // 3. Generate token
+            let token = createToken({ ...login[0].dataValues });
+
+            console.log(login[0].dataValues)
             res.status(200).send({
                 success: true,
                 message: "Auth data success ✅",
-                data: login
+                data: { ...login[0].dataValues, token }
             })
         }
 
@@ -47,7 +59,7 @@ export const regis = async (req, res, next) => {
         req.body.password = Crypto.createHmac("sha256", process.env.HASH_KEY)
             .update(req.body.password)
             .digest("hex");
-            
+
         let result = await users.create(req.body);
 
         console.log(result);

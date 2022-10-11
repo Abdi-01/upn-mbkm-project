749e4c74089817534df086f0ce8600d536d74298
Abdi-01 <abdialghi@gmail.com>
2022-10-11 20:19:30 +0700

    nextjs : import flowbiet react CDN

--- projects/client/components/Meta.jsx
+++ projects/client/components/Meta.jsx
@@ -7,8 +7,11 @@ const Meta = (props) => {
 
     return <Head>
         <title>{props.title}</title>
-        <meta property='og:title' content='Rubick | Welcome'/>
-        <meta property='og:description' content='Dashboard Web App'/>
+        <meta property='og:title' content='Rubick | Welcome' />
+        <meta property='og:description' content='Dashboard Web App' />
+        {/* Import Flowbite CDN */}
+        <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
+        <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
     </Head>
 }
 
--- projects/client/pages/dashboard/information.jsx
+++ projects/client/pages/dashboard/information.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Meta from '../../components/Meta';
 
 const Main = (props) => {
 
@@ -39,19 +40,24 @@ const Main = (props) => {
                     </p>
                     <a href="#" className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600">
                         Read more
-                        <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </a>
                 </div>
             </div>
         });
     }
 
-    return <div className='my-5'>
-        <p className='text-4xl'>Information page</p>
-        <div className='mt-10 flex flex-wrap justify-around'>
-            {printData()}
+    return <>
+        <Meta title="Information" />
+        <div className='my-5'>
+            <p className='text-4xl'>Information page</p>
+
+            {/* Info Card */}
+            <div className='mt-10 flex flex-wrap justify-around'>
+                {printData()}
+            </div>
         </div>
-    </div>
+    </>
 }
 
 export default Main;

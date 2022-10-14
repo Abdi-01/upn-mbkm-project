041a51717dd992d26ed9aa975db8e6e13f05dfff
Abdi-01 <abdialghi@gmail.com>
2022-10-14 14:45:37 +0700

    nextjs : create form filter

--- projects/client/pages/dashboard/pm-merdeka/index.jsx
+++ projects/client/pages/dashboard/pm-merdeka/index.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Meta from '../../../components/Meta';
 import { API_URL } from '../../../helper';
-import { HiRefresh } from 'react-icons/hi';
+import { HiRefresh, HiPrinter } from 'react-icons/hi';
 import axios from 'axios';
 
 const Main = (props) => {
@@ -60,11 +60,29 @@ const Main = (props) => {
                     <h2 className='font-medium text-base'>
                         PM-Merdeka
                     </h2>
-                    <button type="button" className="text-white flex items-center bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <HiRefresh size={24} className="mr-3" /> Refresh
-                    </button>
+                    <div className='flex items-center'>
+                        <button type="button" className="text-white flex items-center bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <HiPrinter size={24} className="mr-3" /> Print
+                        </button>
+                        <button type="button" onClick={getData} className="text-white flex items-center bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <HiRefresh size={24} className="mr-3" /> Refresh
+                        </button>
+                    </div>
                 </div>
                 <hr className='my-5' style={{ border: '1.5px solid black' }} />
+                <div id='filter-data' className='flex p-5'>
+                    <div className='w-1/5'>
+                        <label for="website-admin" className="block mb-2 text-sm font-medium text-gray-900">Cari berdasarkan</label>
+                        <div className="flex">
+                            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300">
+                                
+                            </span>
+                            <input type="text" id="website-admin"
+                                className="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5"
+                                placeholder="Cari..." />
+                        </div>
+                    </div>
+                </div>
                 <div className="overflow-x-auto relative shadow-md sm:rounded-lg p-5">
                     <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-white uppercase bg-gray-400">

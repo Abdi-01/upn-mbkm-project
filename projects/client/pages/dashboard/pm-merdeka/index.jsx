0207679977ea2e2df4e93e1a79d90acd816271d4
Abdi-01 <abdialghi@gmail.com>
2022-10-14 15:51:06 +0700

    nextjs : create form filter

--- projects/client/pages/dashboard/pm-merdeka/index.jsx
+++ projects/client/pages/dashboard/pm-merdeka/index.jsx
@@ -10,6 +10,11 @@ const Main = (props) => {
     // Menampung data yang didapat dari API
     const [data, setData] = React.useState([]); // menentukan default value pada state sesuai data yang akan digunakan
 
+    // Menampung data parameter pencarian dari select option
+    const [searchParam, setSearchParam] = React.useState('type');
+    const [searchValue, setSearchValue] = React.useState('');
+
+
     // Menyiapkan fungsi untuk get data dari API saat pertama kali page dirender
     const getData = async () => {
         try {
@@ -75,13 +80,15 @@ const Main = (props) => {
                         <label for="website-admin" className="block mb-2 text-sm font-medium text-gray-900">Cari berdasarkan</label>
                         <div className="flex">
                             <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 rounded-l-md border border-r-0 border-gray-300">
-                                <select className="bg-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5">
+                                <select value={searchParam} onChange={(event) => setSearchParam(event.target.value)} className="bg-gray-200 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5">
                                     <option value="type">Type</option>
                                     <option value="mahasiswa">Mahasiswa</option>
                                     <option value="kode_jadwal">Kode Jadwal</option>
                                 </select>
                             </span>
                             <input type="text" id="website-admin"
+                                value={searchValue}
+                                onChange={(event) => setSearchValue(event.target.value)}
                                 className="rounded-none rounded-r-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5"
                                 placeholder="Cari..." />
                         </div>
